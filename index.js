1408aeb7b101c83487269135830c22a111a2e93a
hosy <hosy@nurigo.net>
2017-10-20 11:52:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.get('/healthcheck', function (req, res) {
   res.send('checking health... OK!');
 });
 
+app.get('/user3', function (req, res) {
+  res.send('Hello user3');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
